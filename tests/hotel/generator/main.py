2a2b45f7f86e9fae14276e1691e825959ae51cca
Pablo <pablofcastro@gmail.com>
2025-10-06 16:34:38 -0300

    some imrpovenment in the generator

--- tests/hotel/generator/main.py
+++ tests/hotel/generator/main.py
@@ -1,27 +1,40 @@
+import sys
 from sys import argv
 from num2words import num2words
 from uplts import plts, perception, kh 
+import pathlib # for managing paths
+
 
 if __name__ == '__main__':
 
-    doors         = len(argv)-3
-    probabilities = list(map(float, argv[1:-1]))
-    mode          = argv[-1]
+    doors         = len(argv)-4
+    if doors < 4 :
+        print("Error: the number of doors have to be greater than 3")
+        print(f'  use: {argv[0]} <success:float> <prob1:float> <prob2:float> ... <probn:float> tango|pericon|zamba <path>')
+        sys.exit(1) # Exit with an error code
+
+    probabilities = list(map(float, argv[1:-2]))
+    mode          = argv[-2]
+    
 
     if doors > 0:
 
-        fname = f'{mode}-' + num2words(doors) + '.kr'
-
-        try:
-            ffile = open(fname,'x')
-
+        fname = f'{mode}-{doors}-{probabilities[0]}.kr'
+        path          = pathlib.Path(argv[-1]+"/"+fname)
+        with path.open(mode="w", encoding="utf-8") as ffile:
             ffile.write(f"{plts(doors,probabilities)}")
             ffile.write(f"{perception(doors,mode)}")
             ffile.write(f"{kh(success=probabilities[0])}")
+#        try:
+#           ffile = open(fname,'x')
+#
+#            ffile.write(f"{plts(doors,probabilities)}")
+#            ffile.write(f"{perception(doors,mode)}")
+#            ffile.write(f"{kh(success=probabilities[0])}")
 
-        except FileExistsError:
-            print(f'the file {fname} already exists')
+#        except FileExistsError:
+ #           print(f'the file {fname} already exists')
 
     else:
         print(f'error: incorrect number of arguments for {argv[0]}')
-        print(f'  use: {argv[0]} <success:float> <prob1:float> <prob2:float> ... <probn:float> tango|pericon|zamba')
+        print(f'  use: {argv[0]} <success:float> <prob1:float> <prob2:float> ... <probn:float> tango|pericon|zamba <path>')
